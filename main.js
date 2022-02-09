8156272cf6bebb7076e9212068503782500d30aa
Sergio Borbone <sergioborbone@gmail.com>
2022-02-09 17:14:00 +0100

    Mostro del testo in un h1 e un'immagine in un div

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = new Vue(
+
+    {
+        el: '#vue-root',
+        data: {
+            text: 'Hello, this is a test!',
+            img: 'img/peepo-hacker.jpeg',
+        },
+
+    }
+
+);
